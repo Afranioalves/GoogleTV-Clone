d5c1850f9d9bfa2216f73a88d459ed90895b742d
Afranio Alves <afranio777alves@gmail.com>
2023-02-12 23:11:17 +0100

    style home added

---
+++ src/styles/home.style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#1F1F1F',
+        flex:1, 
+        paddingHorizontal:16,
+        paddingTop:24
+    }
+})
+
+export default styles
