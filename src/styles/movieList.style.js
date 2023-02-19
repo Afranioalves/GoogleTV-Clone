f0280d038fb78711280476dc1c641b895c8dfdda
Afranio Alves <afranio777alves@gmail.com>
2023-02-19 21:16:01 +0100

    movie list style added

---
+++ src/styles/movieList.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#1F1F1F',
+        flex:1, 
+        paddingTop:36
+    },
+    scrollViewContainer:{
+        paddingTop:24,
+        paddingRight:36
+        
+    }
+})
+
+export default styles
