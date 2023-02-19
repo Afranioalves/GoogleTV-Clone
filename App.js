7cecb19456bdcbb7c134bb6ab03d82225760f7f5
Afranio Alves <afranio777alves@gmail.com>
2023-02-19 21:16:50 +0100

    new page added

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/screens/home';
+import MoviesList from './src/screens/moviesList';
+import Details from './src/screens/details';
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown:false}}>
-          <Stack.Screen name="Home" component={Home}/>       
-      </Stack.Navigator>
-  </NavigationContainer>
+   
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Details' screenOptions={{headerShown:false}}>
+            <Stack.Screen name="Home" component={Home}/> 
+            <Stack.Screen name="List" component={MoviesList}/>   
+            <Stack.Screen name="Details" component={Details}/>       
+        </Stack.Navigator>
+      </NavigationContainer>
+   
+
   );
 }
 
