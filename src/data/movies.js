19303747ef22f77eed248593649f429ef4c2d161
Afranio Alves <afranio777alves@gmail.com>
2023-02-24 03:59:11 +0100

    fake data added

---
+++ src/data/movies.js
@@ -0,0 +1,102 @@
+import Aquaman from '../../assets/img/aquaman.jpg'
+import Venom from '../../assets/img/venom.jpg'
+import Adam from '../../assets/img/adam_black.jpg'
+import Panther from '../../assets/img/panther.jpg'
+import Player from '../../assets/img/player.jpg'
+import Transformer from '../../assets/img/Transformers.jpg'
+import Bumbeblee from '../../assets/img/bumble.jpg'
+import Spider from '../../assets/img/spider.jpg'
+import King from '../../assets/img/king.jpg'
+
+export const films = [
+    {   
+        id:1,
+        name:'Black Adam',
+        cover: Adam,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:2,
+        name:'The Player One',
+        cover: Player,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:3,
+        name:'Black Panther',
+        cover: Panther,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:4,
+        name:'Spider Man: No way Home',
+        cover: Spider,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:5,
+        name:'Bumbeblee',
+        cover: Bumbeblee,
+        price: '1 800,00 kz',
+        count:467,
+        vote:5.5
+    },
+    {   
+        id:6,
+        name:'Venom',
+        cover: Venom,
+        price: '2 400,00 kz',
+        count:45567,
+        vote:7
+    },
+    {   
+        id:7,
+        name:'Aquaman',
+        cover: Aquaman,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:8,
+        name:'Transformer Apocalipse',
+        cover: Transformer,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:9,
+        name:'Woman King',
+        cover: King,
+        price: '1 800,00 kz',
+        count:45567,
+        vote:7.5
+    },
+    {   
+        id:10,
+        name:'Bumbeblee',
+        cover: Bumbeblee,
+        price: '1 800,00 kz',
+        count:467,
+        vote:5.5
+    },
+    {   
+        id:11,
+        name:'Venom',
+        cover: Venom,
+        price: '2 400,00 kz',
+        count:45567,
+        vote:7
+    },
+]
+
+
