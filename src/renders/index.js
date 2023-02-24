5617ad55a7b6f5471e0b918a1f2b9ace83bcd041
Afranio Alves <afranio777alves@gmail.com>
2023-02-24 03:59:34 +0100

    render movie components added

--- src/renders/index.js
+++ src/renders/index.js
@@ -1,23 +1,37 @@
 import { BASE_IMG } from "../config/api"
 import Movie from "../components/movie"
 
-export const MovieRender = (data) =>{
+export const MovieRender = (data, navigate) =>{
     return data.map((film)=>{
         return(
           <Movie
             key={film.id} 
-            poster={`${BASE_IMG}${film.backdrop_path}`}
+            poster={film.backdrop_path}
             price={film.vote_count.toLocaleString("de-DE",{ maximumSignificantDigits: 3 })+' kz'}
             vote={film.vote_average}
             count={film.vote_count}
+            data={film}
+            screen="Home"
+            navigate={navigate}
           />
         )
       })
   }
 
-  {
-/*
-    <Movie poster={Spider} price='1 800,00 kz'/>
-                <Movie poster={Bumbeblee}/>
-                <Movie poster={Adam}/>*/
+  export const MovieRenderLocal = (data, navigate) =>{
+    return data.map((film)=>{
+        return(
+          <Movie
+            key={film.id} 
+            poster={film.cover}
+            price={film.price}
+            vote={film.vote}
+            count={film.count}
+            data={film}
+            screen="Home"
+            navigate={navigate}
+          />
+        )
+      })
   }
+
